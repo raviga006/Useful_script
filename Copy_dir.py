acf58df270eae4bbc73f988f2fe8b88a98fdab47
Ravie kumarr Sapram <70814918+raviga006@users.noreply.github.com>
2022-12-27 13:01:58 +0530

    Copy_dir
    Copy files and multiple folders  using python

---
+++ Copy_dir.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+source_dir = r"C:\\project1\\TST\\tg-\\src\\tst\\we\\cur\\tst"
+destination_dir = r"list"
+list = ["bitbucket-01",Git-01]
+for items in list:
+    path = os.path.join("C:\\project1\\TST\\tg-\\src\\tst\\we\\cur\\tst"", "test.yml" )
+    shutil.copytree("C:\\project1\\TST\\tg-\\src\\tst\\we\\cur\\tst","list",dirs_exist_ok=True)
